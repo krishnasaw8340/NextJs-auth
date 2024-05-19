877c5804a326b0519388f9660025d71db27e8cd6
Krishna Saw <krishnakrsaw8340@gmail.com>
2024-05-19 22:22:58 +0530

    mailtrap-setup

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -5,17 +5,47 @@ import bcryptjs from 'bcryptjs'
 export const sendMail = async({email, emailType, userId}:any) => {
     try{
         const hashedToken = await bcryptjs.hash(userId.toString(), 10)
-        await User.findByIdAndUpdate(userId, 
+        if(emailType === "VERIFY")
             {
-                verifyToken:hashedToken,
-                verifyTokenExpiry:Date.now() + 36000
-            }, {new: true, runValidators: true}
-        )
+                await User.findByIdAndUpdate(userId,
+                    {verifyToken:hashedToken,
+                        verifyTokenExpiry:Date.now() + 3600000
+                    }
+                )
+            }
+        else if(emailType === "RESET")
+            {
+                await User.findByIdAndUpdate(userId,
+                    {forgotPasswordToken: hashedToken,
+                        forgotPasswordTokenExpiry: Date.now() + 360000
+                    }
+                )
+
+            }
+        // await User.findByIdAndUpdate(userId, 
+        //     {
+        //         verifyToken:hashedToken,
+        //         verifyTokenExpiry:Date.now() + 36000
+        //     }, {new: true, runValidators: true}
+        // )
+        var transport = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+              user: "3e3759719f5e94",
+              pass: "2cb4a230f9b007"
+            }
+          });
+
+          const mailOptions = {
+            from:'infosecmenu@gmail.com',
+            to: email,
+            subject: emailType === 'VERIFY' ? 'Verify your email'  : 'Reset your password'
+          }
     }
     catch(error:any)
     {
         throw new Error(error.message);
-        
 
     }
 }
