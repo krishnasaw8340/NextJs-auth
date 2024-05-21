fe7cac1dc59c4e63673529bd55b876c7e61c84a9
Krishna Saw <krishnakrsaw8340@gmail.com>
2024-05-22 00:16:12 +0530

    dashboard-style-added

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
 
-export default function UserProfile ({params}:any) {
+export default function UserProfile({ params }: any) {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-             <h1>Profile</h1>
-             <hr/>
-             <p className="text-4xl">Profile page Different 1 [id]  : </p>
-             <span className="p-2 rounded bg-rounded-md text-orange-500">{params.id}</span>
-             <Link href='/profile'>Home</Link>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8">
+            <h1 className="text-3xl font-bold mb-4">Profile</h1>
+            <hr className="w-16 border-t-2 mb-4 border-gray-400" />
+            <p className="text-2xl mb-4">Profile Page Different 1 [id]:</p>
+            <div className="p-2 bg-orange-500 text-white rounded-md">
+                {params.id}
+            </div>
+            <Link href='/profile' className="text-blue-500 hover:underline mt-4">
+                Back to Home
+            </Link>
         </div>
-    )
+    );
 }
